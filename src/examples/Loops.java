c67b6bd47df4c4d7473705a3063ae8cdb610e313
Tyler J Holm <tyler.holm97@gmail.com>
2021-04-10 15:04:35 -0500

    Add Module 7 - Loop Examples

---
+++ src/examples/Loops.java
@@ -0,0 +1,21 @@
+/**********
+ * Module 7
+ * Examples for Loops
+ */
+package examples;
+
+public class Loops {
+	public static void main(String[] args) {
+		//For Loop
+		for(int i=1; i<=3; i++) {
+			System.out.println("For loop iteration count: " + i);
+		}
+		
+		//While Loop
+		int j = 1;
+		while(j<=3) {
+			System.out.println("While loop iteration count: " + j);
+			j++;
+		}
+	}
+}
