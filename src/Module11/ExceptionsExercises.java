52fafd936d9643831a0cd11b5303ce152d0f84bc
Cassie <cassie.holm97@gmail.com>
2021-04-12 21:12:18 -0500

    Suppress serial warning

--- src/Module11/ExceptionsExercises.java
+++ src/Module11/ExceptionsExercises.java
@@ -1,6 +1,8 @@
 package Module11;
 
+@SuppressWarnings("serial")
 public class ExceptionsExercises extends Exception{
+
 	public String getMessage() {
 		return "You cannot input an odd number.";
 	}
